fd163c0e21902aa49272e1670a590d8f95d2b342
Lester Gabriel <43458048+LesterCordero@users.noreply.github.com>
2018-09-28 12:49:27 -0600

    Para que emma la contemple uwu

--- Tarea4/func.asm
+++ Tarea4/func.asm
@@ -17,10 +17,10 @@
 ; esta seccion realiza la comparacion entre cada char de ambas hileras usando desplazamientos (r12)
 ; rdi = ubicacion de hilera 1, rsi = ubicacion de hilera 2, r12 = desplazamiento relativo
 %macro compareChars 0
-	setPuntero rbp-2		; r12 ahora tiene el contador para la hilera 2
+	setPuntero rbp-2	; r12 ahora tiene el contador para la hilera 2
 	mov al, byte[rsi+r12]   ; copia el char del de hile2 y lo guarda en "AL"
 	setPuntero rbp-1        ; r12 ahora tiene el contador para la hilera 1
-	inc byte[rbp-1]			; incremente el contador para el puntero de la hilera1 para la siguiente iteracion
+	inc byte[rbp-1]		; incremente el contador para el puntero de la hilera1 para la siguiente iteracion
 	cmp byte[rdi+r12], al	; compara que el char guardado sea igual con el que apunta la hilera1
 %endmacro
 
@@ -28,8 +28,8 @@
 %macro epilogo 0
 	mov rax, 0              ; limpia RAX de posible basura
 	mov al, byte[rbp-3]     ; coloca el resultado en rax
-	add rsp, 3         		; elimina los 3 "bytes" reservados 
-	pop rbp            		; recupera el base pointer
+	add rsp, 3         	; elimina los 3 "bytes" reservados 
+	pop rbp            	; recupera el base pointer
 %endmacro
 
 SECTION .text
@@ -44,18 +44,18 @@ subseq:
 compare_subseq:
 
 	; evalua si no es subhilera cuando la hilera 1 llego a su fin
-	setPuntero rbp-1		; r12 ahora tiene el contador para la hilera 1
+	setPuntero rbp-1	; r12 ahora tiene el contador para la hilera 1
 	cmp byte[rdi+r12], 0	; si la hilera1 lego a su fin, NO es subsecuencia
-	je final_subseq			; no es subsecuencia, termine el programa
+	je final_subseq		; no es subsecuencia, termine el programa
 	
-	compareChars			; llame al comparador de chars
+	compareChars		; llame al comparador de chars
 	
 	; realiza el salto para seguir buscando el siguiente char en hilera en caso de no coincidir los chars
-	jne compare_subseq		; si no son iguales, repita (ya el puntero apunta al siguiente)
+	jne compare_subseq	; si no son iguales, repita (ya el puntero apunta al siguiente)
 	
 	; acerto un char, sumele al contador de
-	inc byte[rbp-2]			; incremente el contador para el puntero de la hilera2
-	setPuntero rbp-2		; r12 ahora tiene el contador de la hilera 2
+	inc byte[rbp-2]		; incremente el contador para el puntero de la hilera2
+	setPuntero rbp-2	; r12 ahora tiene el contador de la hilera 2
 	cmp byte[rsi+r12], 0	; si la hilera 2 llega a su fin, significa que SI es subsecuencia
 	jne compare_subseq   	; si no ha llegado a su fin, siga comparando
 	
@@ -65,34 +65,34 @@ compare_subseq:
 ; --EPILOGO --	
 final_subseq:
 
-	epilogo					; llame al epilogo común
-	ret						; retorna el control al programa de C
+	epilogo			; llame al epilogo común
+	ret			; retorna el control al programa de C
 	
 ; -- SUBHILERA -------------------------------------------------------------------------------------	
 GLOBAL subhile                             
 subhile:
 ; --PROLOGO --
 
-	prologo					; llame al prologo común
+	prologo			; llame al prologo común
 	
 ; --CUERPO --
 fallo_subhile:
 
-	mov byte[rbp-2], 0		; la hilera no estaba consecutiva, devuelva el contador a 0
+	mov byte[rbp-2], 0	; la hilera no estaba consecutiva, devuelva el contador a 0
 	
 compare_subhile:
 
 	; los jumps ahora son respecto a subhilera
-	setPuntero rbp-1		; r12 ahora tiene el contador para la hilera 1
+	setPuntero rbp-1	; r12 ahora tiene el contador para la hilera 1
 	cmp byte[rdi+r12], 0	; si la hilera1 lego a su fin, NO es subhilera
-	je final_subhile		; no es subhilera, termine el programa
+	je final_subhile	; no es subhilera, termine el programa
 	
-	compareChars			; llame al comparador de chars
-	jne fallo_subhile		; igual que subseq, repita si no coinciden
+	compareChars		; llame al comparador de chars
+	jne fallo_subhile	; igual que subseq, repita si no coinciden
 	
 	; practicamente igual que subsecuencia, pero con la diferencia de que si falla, setee el contador en 0 de nuevo
-	inc byte[rbp-2]			; incremente el contador para el puntero de la hilera2
-	setPuntero rbp-2		; r12 ahora tiene el contador de la hilera 2
+	inc byte[rbp-2]		; incremente el contador para el puntero de la hilera2
+	setPuntero rbp-2	; r12 ahora tiene el contador de la hilera 2
 	cmp byte[rsi+r12], 0	; si la hilera 2 llega a su fin, significa que SI es subsecuencia
 	jne compare_subhile   	; si no ha llegado a su fin, siga comparando
 	
@@ -101,5 +101,5 @@ compare_subhile:
 ; --EPILOGO --	
 final_subhile:
 
-	epilogo					; llame al epilogo común
-	ret						; retorna el control al programa de C
+	epilogo			; llame al epilogo común
+	ret			; retorna el control al programa de C
